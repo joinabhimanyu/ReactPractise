fba4417bf1eea3bcae0665c8b765c2ca6e7c0bf6
Chakraborty <Abhimanyu.Chakraborty@changehealthcare.com>
2019-04-09 10:58:46 -0500

    changes to gitignore for lib

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import PluginEditor from './customEditor/customEditor';
+import composeDecorators from './utils/composeDecorators';
+import { initialState } from './utils/constants';
+
+export { PluginEditor, composeDecorators, initialState }
